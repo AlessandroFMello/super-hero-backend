1561d51289aa33b75f2721812d68fa40ba8f8550
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-19 13:46:08 -0300

    feat: :sparkles: Creates errorMiddleware to handle errors

---
+++ src/api/middlewares/errorMiddleware.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+const errorMiddleware = (err, _req, res, _next) => {
+  console.log(err);
+
+  return res.status(500).json({ err, error: 'Internal Server Error' });
+};
+
+module.exports = errorMiddleware;
