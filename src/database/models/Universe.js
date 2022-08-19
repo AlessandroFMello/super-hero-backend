c185a918fdd9929a47a3ace502e8c1a9a889e644
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-19 14:17:40 -0300

    feat: :sparkles: Creates Universe models

---
+++ src/database/models/Universe.js
@@ -0,0 +1,31 @@
+const Universe = (sequelize, DataTypes) => {
+  const universe = sequelize.define('Universe', {
+    id: {
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    universe: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    underscored: true,
+    sequelize,
+    modelName: 'Universe',
+    tableName: 'universes',
+    timestamps: false,
+  });
+
+  universe.associate = (models) => {
+    universe.hasMany(
+      models.Hero,
+      { foreignKey: 'universe', as: 'heroUniverse' },
+    );
+  };
+
+  return universe;
+};
+
+module.exports = Universe;
