4b8a491f57e32477729f8f66d62dba85b48e0e75
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-22 23:05:26 -0300

    feat: :sparkles: Implements getById method in heroesService

--- src/api/services/heroesService.js
+++ src/api/services/heroesService.js
@@ -8,10 +8,11 @@ class HeroesService {
     this.NOT_FOUND = 'Hero not found';
 
     this.getAll = this.getAll.bind(this);
+    this.getById = this.getById.bind(this);
   }
 
   async getAll() {
-    const findAllHeroes = await Hero.findAll({
+    const findAllHeroes = await this.heroModel.findAll({
       include: { model: Universe, as: 'heroUniverse' },
     });
 
@@ -19,6 +20,18 @@ class HeroesService {
 
     return { code: 200, allHeroes: findAllHeroes };
   }
+
+  async getById(id) {
+    const findHeroById = await this.heroModel.findOne({
+      where: { id },
+      include: { model: Universe, as: 'heroUniverse' },
+    });
+
+    if (!findHeroById) return { code: 404, message: this.NOT_FOUND };
+
+    return { code: 200, hero: findHeroById };
+  }
+
 }
 
 module.exports = HeroesService;
